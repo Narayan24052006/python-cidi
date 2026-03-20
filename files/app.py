d40d41d3875b0dffc9b0c91301d62308518bda39
CHINMAI SRI NARAYAN KOTHAMASU <kcsn@Narayans-MacBook-Air.local>
2026-03-20 18:11:40 +0530

    Fix PEP8 linting errors

--- files/app.py
+++ files/app.py
@@ -1,8 +1,11 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 if __name__ == "__main__":
     print("Application is running...")
+# (Make sure there is one empty line here at the very bottom)
--- files/test.py
+++ files/test.py
@@ -1,7 +1,10 @@
 import app
 
+
 def test_add():
     assert app.add(2, 3) == 5
 
+
 def test_subtract():
     assert app.subtract(5, 3) == 2
+# (Make sure there is one empty line here at the very bottom)
